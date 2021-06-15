a0b581de3a47c904e802061b8f5f88164f25e906
Cesar <30559667+CesarAroon@users.noreply.github.com>
2021-06-15 09:36:17 -0500

    Add files via upload

---
+++ triangulo isosceles equilatero.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+    int angulo1;
+    int angulo2;
+    int angulo3;
+    cout << "ingrese el primer angulo" << endl;
+    cin >> angulo1;
+    cout << "ingrese el primer angulo" << endl;
+    cin >> angulo2;
+    cout << "ingrese el primer angulo" << endl;
+    cin >> angulo3;
+  
+    if ((angulo1 < 100 && angulo2 < 100) || (angulo2 < 100 && angulo3 < 100) || (angulo1 < 100 && angulo3 < 100)) {
+
+        cout << "el triangulo es Entangulo " << endl;
+    }
+
+
+    if (angulo1 == 100  || angulo2 == 100  || angulo3 == 100) {
+
+        cout << "el triangulo es Suicangulo " << endl;
+    }
+
+    if (angulo1 > 100 || angulo2 > 100 || angulo3 >  100) {
+
+        cout << "el triangulo es Raikangulo " << endl;
+    }
+  
+    return 0;
+}
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+    int angulo1;
+    int angulo2;
+    int angulo3;
+    cout << "ingrese el primer angulo" << endl;
+    cin >> angulo1;
+    cout << "ingrese el primer angulo" << endl;
+    cin >> angulo2;
+    cout << "ingrese el primer angulo" << endl;
+    cin >> angulo3;
+  
+    if ((angulo1 < 100 && angulo2 < 100) || (angulo2 < 100 && angulo3 < 100) || (angulo1 < 100 && angulo3 < 100)) {
+
+        cout << "el triangulo es Entangulo " << endl;
+    }
+
+
+    if (angulo1 == 100  || angulo2 == 100  || angulo3 == 100) {
+
+        cout << "el triangulo es Suicangulo " << endl;
+    }
+
+    if (angulo1 > 100 || angulo2 > 100 || angulo3 >  100) {
+
+        cout << "el triangulo es Raikangulo " << endl;
+    }
+  
+    return 0;
+}
